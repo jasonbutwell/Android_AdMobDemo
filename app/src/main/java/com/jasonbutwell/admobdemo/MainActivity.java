400a6f1b4b2c616d9a82efe0894df9726e98d0bb
Jason Butwell <jasonbutwell@gmail.com>
2016-11-10 23:02:44 +0000

    Implented AdMob code to display test ads at the bottom of the display for development and learning purposes only.

--- app/src/main/java/com/jasonbutwell/admobdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/admobdemo/MainActivity.java
@@ -1,13 +1,30 @@
 package com.jasonbutwell.admobdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note as stated by Google:
+
+    // It is against AdMob policy to click on your own live ads. During development and testing, use only test ads.
+    // If you do need to render live ads before launch, avoid clicking on them. If you click on live ads, your AdMob account may be suspended.
+
+    // For more info on Admob and the required set up process please refer to this URL:
+    // https://firebase.google.com/docs/admob/android/quick-start
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ad Initialise code to be called first from within the main activity onCreate() method
+
+        AdView adView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 }
